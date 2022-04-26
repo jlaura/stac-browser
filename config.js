7d92a6b3d18b5345c54253f9f7f813b6af1afe29
Jay Laura <jlaura@usgs.gov>
2022-04-26 06:17:40 -0700

    Updates to v3 alpha 9

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 module.exports = {
-    catalogUrl: null,
+    catalogUrl: "https://6vpdmaqce6.execute-api.us-west-2.amazonaws.com/dev",
     catalogTitle: "STAC Browser",
-    allowExternalAccess: true, // Must be true if catalogUrl is not given
+    allowExternalAccess: false, // Must be true if catalogUrl is not given
     useTileLayerAsFallback: true,
     tileSourceTemplate: null,
     buildTileUrlTemplate: ({href, asset}) => 'https://tiles.rdnt.io/tiles/{z}/{x}/{y}@2x?url=' + encodeURIComponent(asset.href.startsWith('/vsi') ? asset.href : href),
     stacProxyUrl: null,
-    pathPrefix: "/",
-    historyMode: "history",
-    cardViewMode: 'cards',
+    pathPrefix: "/browse-dev",
+    historyMode: "hash",
+    cardViewMode: 'list',
     showThumbnailsAsAssets: false,
     stacLint: true,
     geoTiffResolution: 128,
     redirectLegacyUrls: false,
-    itemsPerPage: 12,
+    itemsPerPage: 24,
     crossOriginMedia: null,
     requestHeaders: {},
     requestQueryParameters: {}
